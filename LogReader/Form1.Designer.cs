6ffa5dd3befb5bc899827fc77645b93d12548222
Petty <petty4545@hotmail.com>
2013-09-26 09:03:36 +0200

    First Commit
    v0.1

---
+++ LogReader/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace LogReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Error = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ProcessId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Namespace = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Message = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Stacktrace = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblFileName = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.LogCountValue = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblErrorCount = new System.Windows.Forms.ToolStripStatusLabel();
+            this.ErrorCountValue = new System.Windows.Forms.ToolStripStatusLabel();
+            this.filesListBox = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Error,
+            this.Time,
+            this.ProcessId,
+            this.Namespace,
+            this.Message,
+            this.Stacktrace});
+            this.dataGridView1.Location = new System.Drawing.Point(211, 16);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(930, 576);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.RowPostPaint += new System.Windows.Forms.DataGridViewRowPostPaintEventHandler(this.dataGridView1_RowPostPaint);
+            // 
+            // Error
+            // 
+            this.Error.DataPropertyName = "Error";
+            this.Error.HeaderText = "Error";
+            this.Error.Name = "Error";
+            this.Error.ReadOnly = true;
+            this.Error.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Error.Visible = false;
+            // 
+            // Time
+            // 
+            this.Time.DataPropertyName = "Time";
+            this.Time.HeaderText = "Time";
+            this.Time.Name = "Time";
+            // 
+            // ProcessId
+            // 
+            this.ProcessId.DataPropertyName = "ProcessId";
+            this.ProcessId.HeaderText = "ProcessId";
+            this.ProcessId.Name = "ProcessId";
+            // 
+            // Namespace
+            // 
+            this.Namespace.DataPropertyName = "Namespace";
+            this.Namespace.HeaderText = "Namespace";
+            this.Namespace.Name = "Namespace";
+            // 
+            // Message
+            // 
+            this.Message.DataPropertyName = "Message";
+            this.Message.HeaderText = "Message";
+            this.Message.Name = "Message";
+            // 
+            // Stacktrace
+            // 
+            this.Stacktrace.DataPropertyName = "Stacktrace";
+            this.Stacktrace.HeaderText = "Stacktrace";
+            this.Stacktrace.Name = "Stacktrace";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 568);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(193, 22);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Másik fájl megnyitása";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblFileName,
+            this.toolStripStatusLabel1,
+            this.LogCountValue,
+            this.lblErrorCount,
+            this.ErrorCountValue});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 593);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1153, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(0, 17);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(35, 17);
+            this.toolStripStatusLabel1.Text = "Logs:";
+            // 
+            // LogCountValue
+            // 
+            this.LogCountValue.Name = "LogCountValue";
+            this.LogCountValue.Size = new System.Drawing.Size(0, 17);
+            // 
+            // lblErrorCount
+            // 
+            this.lblErrorCount.Name = "lblErrorCount";
+            this.lblErrorCount.Size = new System.Drawing.Size(40, 17);
+            this.lblErrorCount.Text = "Errors:";
+            // 
+            // ErrorCountValue
+            // 
+            this.ErrorCountValue.Name = "ErrorCountValue";
+            this.ErrorCountValue.Size = new System.Drawing.Size(0, 17);
+            // 
+            // filesListBox
+            // 
+            this.filesListBox.FormattingEnabled = true;
+            this.filesListBox.Location = new System.Drawing.Point(13, 16);
+            this.filesListBox.Name = "filesListBox";
+            this.filesListBox.Size = new System.Drawing.Size(192, 550);
+            this.filesListBox.Sorted = true;
+            this.filesListBox.TabIndex = 5;
+            this.filesListBox.SelectedIndexChanged += new System.EventHandler(this.filesListBox_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1153, 615);
+            this.Controls.Add(this.filesListBox);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Error;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Time;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProcessId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Namespace;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Message;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Stacktrace;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel LogCountValue;
+        private System.Windows.Forms.ToolStripStatusLabel lblErrorCount;
+        private System.Windows.Forms.ToolStripStatusLabel ErrorCountValue;
+        private System.Windows.Forms.ListBox filesListBox;
+        private System.Windows.Forms.ToolStripStatusLabel lblFileName;
+    }
+}
+
---
+++ LogReader/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml;
+using System.Windows.Forms;
+
+namespace LogReader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            List<FileInfo> files = new DirectoryInfo("c:\\Temp\\").GetFiles("*.txt").OrderBy(p => p.CreationTime).ToList();
+            DateTime latest = DateTime.MinValue;
+            string defaultfile = string.Empty;
+            foreach (FileInfo file in files)
+            {
+                string filePath = file.FullName;
+                filesListBox.Items.Add(Path.GetFileNameWithoutExtension(filePath));
+                DateTime time = File.GetLastWriteTime(filePath);
+                if (time > latest)
+                {
+                    latest = time;
+                    defaultfile = filePath;
+                }
+            }
+            OpenFile(Path.GetFileNameWithoutExtension(defaultfile));
+        }
+
+        private void OpenFile(string fileName)
+        {
+            string line = string.Empty;
+            dataGridView1.Rows.Clear();
+
+            //open the Log file for reading
+            TextReader reader = new StreamReader(File.OpenRead(@"c:\Temp\" + fileName + ".txt"));
+            string file = reader.ReadToEnd();
+            if (file.StartsWith("<"))
+            {
+                string xmlString = string.Join("", new String[] { "<Log>", file, "</Log>" });
+                XmlDocument xml = new XmlDocument();
+                xml.LoadXml(xmlString);
+                XmlNodeList list = xml.SelectNodes("Log/LogInfo");
+                XmlNodeList listerror = xml.SelectNodes("Log/LogErrorInfo");
+
+
+                List<LogInfo> logs = new List<LogInfo>();
+
+                foreach (XmlNode node in list)
+                {
+                    LogInfo log = new LogInfo();
+                    //log.Time = DateTime.Parse(node.Attributes["Time"].Value);
+                    log.Time = node.Attributes["Time"].Value;
+                    if (node.Attributes["ProcessID"] != null)
+                        log.ProcessId = node.Attributes["ProcessID"].Value;
+                    if (node.SelectSingleNode("Message") != null)
+                        log.Message = node.SelectSingleNode("Message").InnerText;
+                    else if (node.SelectSingleNode("Entry") != null)
+                        log.Message = node.SelectSingleNode("Entry").InnerText;
+                    logs.Add(log);
+
+                    dataGridView1.Rows.Add("", log.Time, log.ProcessId, log.Namespace, log.Message, log.Stacktrace);
+                }
+
+                foreach (XmlNode node in listerror)
+                {
+                    LogInfo log = new LogInfo();
+                    log.Time = node.Attributes["Time"].Value;
+                    if (node.Attributes["ProcessID"] != null)
+                        log.ProcessId = node.Attributes["ProcessID"].Value;
+                    if (node.SelectSingleNode("Namespace") != null)
+                        log.Namespace = node.SelectSingleNode("Namespace").InnerText;
+                    log.Message = node.SelectSingleNode("Message").InnerText;
+                    if (node.SelectSingleNode("Stacktrace") != null)
+                        log.Stacktrace = node.SelectSingleNode("Stacktrace").InnerText;
+                    logs.Add(log);
+
+                    dataGridView1.Rows.Add("ERROR", log.Time, log.ProcessId, log.Namespace, log.Message, log.Stacktrace);
+                }
+
+                dataGridView1.Sort(dataGridView1.Columns[1],ListSortDirection.Ascending);
+                dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+                LogCountValue.Text = (list.Count + listerror.Count).ToString();
+                ErrorCountValue.Text = listerror.Count.ToString();
+
+                //Close log file
+                reader.Close();
+                lblFileName.Text = fileName;
+            }
+            else
+            {
+                MessageBox.Show("A fájl felépítése nem megfelelő");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                OpenFile(Path.GetFileNameWithoutExtension(openFileDialog1.FileName));
+            }
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            dataGridView1.Width = this.Width - 240;
+            dataGridView1.Height = this.Height - 80;
+        }
+
+        private void dataGridView1_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+            dataGridView1.DefaultCellStyle.SelectionBackColor = Color.FromArgb(60,142,255);
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                if (dataGridView1.Rows[i].Cells[0].Value != null && dataGridView1.Rows[i].Cells[0].Value.Equals("ERROR"))
+                {
+                    //dataGridView1.Rows[i].DefaultCellStyle.ForeColor = Color.Red;
+                    dataGridView1.Rows[i].DefaultCellStyle.BackColor = Color.FromArgb(255,126,126);
+                    dataGridView1.AdvancedCellBorderStyle.All = DataGridViewAdvancedCellBorderStyle.None;
+                }
+            }
+        }
+
+        private void filesListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (filesListBox.SelectedIndex != -1)
+                OpenFile(filesListBox.SelectedItem.ToString());
+        }
+    }
+
+    public class LogInfo
+    {
+        public string Time = string.Empty;
+        public string ProcessId = string.Empty;
+        public string Namespace = string.Empty;
+        public string Message = string.Empty;
+        public string Name = string.Empty;
+        public string Stacktrace = string.Empty;
+    }
+
+
+}
